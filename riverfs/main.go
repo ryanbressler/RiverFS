fdd08f670ccb4958b3d26ce1cea92399fa0f494e
Ryan Bressler <ryanbressler@gmail.com>
2013-09-06 16:17:08 -0600

    fuse cleanup before raft

--- riverfs/main.go
+++ riverfs/main.go
@@ -7,42 +7,42 @@ import (
 
 func main() {
 	var mountpoint string
-	flag.StringVar(&mountpoint, "mountpoint", "hotpotato", "Where to mount the fuse dir.")
+	flag.StringVar(&mountpoint, "mountpoint", "river", "Where to mount the fuse dir.")
 
-	// var target string
-	// flag.StringVar(&target, "target", "", "The target dir")
+	var raftlog string
+	flag.StringVar(&raftlog, "raftlog", "raftlog", "The path to the local raft log.")
 
-	// var me string
-	// flag.StringVar(&me, "me", "http://localhost", "Address for this node.")
+	var me string
+	flag.StringVar(&me, "me", "http://localhost", "Address for this node.")
 
-	// var peerlist string
-	// flag.StringVar(&peerlist, "peers", "", "Text file to load peers from.")
+	var peerlist string
+	flag.StringVar(&peerlist, "peers", "", "Text file to load peers from.")
 
-	// peers := make([]string, 10)
-	// if peerlist != "" {
+	peers := make([]string, 10)
+	if peerlist != "" {
 
-	// 	peerfile, err := os.Open(peerlist)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	tsv := csv.NewReader(peerfile)
-	// 	tsv.Comma = '\t'
-	// 	for {
-	// 		url, err := tsv.Read()
-	// 		if err == io.EOF {
-	// 			break
-	// 		} else if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		peers = append(peers, url[0])
+		peerfile, err := os.Open(peerlist)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tsv := csv.NewReader(peerfile)
+		tsv.Comma = '\t'
+		for {
+			url, err := tsv.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			peers = append(peers, url[0])
 
-	// 	}
-	// 	peerfile.Close()
+		}
+		peerfile.Close()
 
-	// }
+	}
 
 	flag.Parse()
 
-	RiverFS.ServeDir(mountpoint)
+	RiverFS.FuseMount(mountpoint)
 
 }
